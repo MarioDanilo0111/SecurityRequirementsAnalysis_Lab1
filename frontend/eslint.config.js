2c028e24c376db81bf42d49c9947987a3d2a5e4e
Mario Fernández <nilovinilo1@gmail.com>
2025-12-28 16:52:11 +0100

    fix: lint conf

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["dist/**", "node_modules/**"] },
+  { ignores: ["dist/**", "node_modules/**", "test-results/**"] },
 
   // JavaScript / TypeScript rules
   {
